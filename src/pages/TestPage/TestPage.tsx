24dc8a322b444b226d35260f6db390fdec1e7b3f
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2023-08-04 01:52:44 +0300

    endpoints injected

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -2,44 +2,71 @@ import React, { useState, useEffect } from "react";
 
 const TestPage = () => {
   const [creatures, setCreatures] = useState([]);
+  const [gameState, setGameState] = useState({
+    creature: {
+      id: undefined,
+    },
+    battle: {
+      id: undefined,
+      logs: [],
+    },
+  });
   const getCreatures = async () => {
     const creatures = await fetch("https://gapi.nftinit.io/api/get_creature/", {
       method: "POST",
-      mode: "cors",
+      //   mode: "cors",
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Origin": "*",
         Accept: "application/json",
         "Content-Type": "application/json",
-        userInfo: JSON.stringify({
-          token: "12345",
-          character_id: "12",
-        }),
+        // userInfo: JSON.stringify({
+        //   //   token: "12345",
+        //   //   character_id: "12",
+        // }),
       },
-      // body:JSON.stringify({
-
-      // })
+      body: JSON.stringify({
+        token: "12345",
+      }),
     }).then((res) => res.json());
-    // console.log("creatures :>> ", creatures);
-    setCreatures(creatures);
+    console.log("creatures :>> ", creatures);
+    setCreatures(creatures.creature_list);
+
+    setGameState((prevGameState) => ({
+      ...prevGameState,
+      creature: {
+        ...prevGameState.creature,
+        id: creatures.creature_list[0].creature_id,
+      },
+    }));
 
     // burada gelen creatureları setleyeceğiz
   };
   const startBattle = async () => {
     const battle = await fetch("https://gapi.nftinit.io/api/battle_start/", {
       method: "POST",
+      //   mode: "cors",
       headers: {
         Accept: "*/*",
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+
         userInfo: JSON.stringify({
           token: "12345",
           character_id: "12",
         }),
       },
       body: JSON.stringify({
-        creature_id: 6,
+        creature_id: gameState.creature.id,
       }),
     }).then((res) => res.json());
     console.log("battle :>> ", battle);
+    setGameState((prevGameState) => ({
+      ...prevGameState,
+      battle: {
+        ...prevGameState.battle,
+        id: battle.battle_id,
+      },
+    }));
 
     // burada battle idyi setleyeceğiz
   };
@@ -55,11 +82,19 @@ const TestPage = () => {
         }),
       },
       body: JSON.stringify({
-        battle_id: "",
+        battle_id: gameState.battle.id,
         skill_id: 11,
       }),
     }).then((res) => res.json());
     console.log("attack :>> ", attack);
+    setGameState((prevGameState: any) => ({
+      ...prevGameState,
+      battle: {
+        ...prevGameState.battle,
+        // logs: [...prevGameState.battle.logs, ...attack.battle_logs],
+        logs: [...attack.battle_logs],
+      },
+    }));
     // burada logları setleyeceğiz
   };
 
@@ -67,32 +102,69 @@ const TestPage = () => {
     getCreatures();
   }, []);
 
-  const handleChangeSelection = (selection: any) => {};
+  const handleChangeSelection = (creatureId: any) => {
+    console.log("girdi");
+    setGameState((prevGameState) => ({
+      ...prevGameState,
+      creature: {
+        ...prevGameState.creature,
+        id: creatureId,
+      },
+    }));
+  };
+  useEffect(() => {
+    console.log("gameState:>> ", gameState);
+  }, [gameState]);
 
   return (
     <div className="flex justify-center items-center">
       <div className="border p-4 flex flex-col gap-8">
-        <select name="" id="">
-          {creatures.map((option: any) => {
+        <select
+          className="text-black"
+          name=""
+          id=""
+          value={gameState.creature.id}
+          onChange={(e) => handleChangeSelection(e.target.value)}
+        >
+          {creatures?.map((option: any) => {
             return (
               <option
-                onChange={() => handleChangeSelection(option.id)}
-                value=""
+                className="text-black"
+                key={option.creature_id}
+                value={option.creature_id}
               >
-                {}
+                {option.name}
               </option>
             );
           })}
-          <option value="">solucan</option>
-          <option value="">aslan</option>
+          {/* <option key={2} value={3}>
+            {"aslan"}
+          </option> */}
         </select>
 
-        <button className="border p-2 rounded border-zinc-50 ">
+        <button
+          onClick={() => startBattle()}
+          className="border p-2 rounded border-zinc-50 "
+        >
           Start Battle
         </button>
-        <button className="border p-2 rounded border-zinc-50 ">Attack</button>
+        <button
+          onClick={() => attackBattle()}
+          className="border p-2 rounded border-zinc-50 "
+        >
+          Attack
+        </button>
       </div>
-      <div>Loglar buraya</div>
+      <div>
+        {gameState.battle.logs?.map((log: any) => {
+          return (
+            <ul>
+              <li>{`${log.description}`}</li>
+              {/* <div>{log.description}</div> */}
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 };
